10cc1e7ed57b8c183b6342023b61d30f785ebd92
NicholasZeitoun <nzeit8003@gmail.com>
2020-06-06 20:27:08 +1000

    Added review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_065727) do
+ActiveRecord::Schema.define(version: 2020_06_06_102539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2020_05_17_065727) do
   create_table "newsletter_forms", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "content"
+    t.string "email"
+    t.boolean "mailist"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
