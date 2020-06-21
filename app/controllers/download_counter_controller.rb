e44bac76c5809bfde5174887896a499b97794efd
alexander-rivaldy <alexanderrivaldy@gmail.com>
2020-06-21 15:30:51 +1000

    save name and email for download

--- app/controllers/download_counter_controller.rb
+++ app/controllers/download_counter_controller.rb
@@ -9,14 +9,12 @@ class DownloadCounterController < ApplicationController
 
   def create
     @download_counter = DownloadCounter.create(download_counter_params)
-    @download_counter.doc_type = params[:doc_type]
-    @download_counter.counter = 1
-    @download_counter.save
 
-    # if (params[:doc_type].eql? "rulebook")
-    #     redirect_to "https://uploads-ssl.webflow.com/5d79944c26acc6f0a7e96822/5ec5f77e9818918b2003280b_YOHOHO-Rulebook-verbose.pdf", :notice => 'Thanks for dowloading Yohoho Rulebook!'
-    # else
-    #     redirect_to "https://uploads-ssl.webflow.com/5d79944c26acc6f0a7e96822/5ec5363498978d6c3b8631ac_YOHOHO-Print-n-play.pdf", :notice => 'Thanks for dowloading Yohoho Print-n-play!'
-    # end
+    if @download_counter.save
+      redirect_to "https://uploads-ssl.webflow.com/5d79944c26acc6f0a7e96822/5eeeec75299cb9d2e2e7945c_StolenTreasure-Print-n-Play-and-rules.pdf", :notice => 'Thanks for downloading Stolen Treasure! Have fun!'
+    else
+      flash[:danger] = "Invalid values for form!"
+      redirect_to root_path
+    end
   end
 end
--- app/models/download_counter.rb
+++ app/models/download_counter.rb
@@ -1,2 +1,9 @@
 class DownloadCounter < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: URI::MailTo::EMAIL_REGEXP,
+    :message => 'Invalid e-mail! Please provide a valid e-mail address' },
+    uniqueness: { case_sensitive: false }
+  
 end
