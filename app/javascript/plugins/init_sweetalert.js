acb18cc26072fa6fd779eaaabdfbb81fe46f6197
NicholasZeitoun <nzeit8003@gmail.com>
2020-06-21 11:33:00 +1000

    Changes assets to the stolen treasure

---
+++ app/javascript/plugins/init_sweetalert.js
@@ -0,0 +1,27 @@
+import swal from 'sweetalert';
+
+const initSweetalert = (selector, options = {}) => {
+  const swalButton = document.querySelector(selector);
+  if (swalButton) { // protect other pages
+    swalButton.addEventListener('click', () => {
+      swal(options);
+    });
+  }
+};
+
+export { initSweetalert };
+
+
+// app/javascript/plugins/init_sweetalert.js
+// import swal from 'sweetalert';
+
+// const initSweetalert = (selector, options = {}, callback = () => {}) => {
+//   const swalButton = document.querySelector(selector);
+//   if (swalButton) { // protect other pages
+//     swalButton.addEventListener('click', () => {
+//       swal(options).then(callback); // <-- add the `.then(callback)`
+//     });
+//   }
+// };
+
+// export { initSweetalert };
